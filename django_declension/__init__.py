53b5bedf57f556b6323e529d492ee1f82c849928
Andrii Kostenko <andrey@kostenko.name>
2011-07-31 17:17:58 +0200

    Tested declension in production
    Returns None if yandex blocks API.
    Timeout

--- django_declension/__init__.py
+++ django_declension/__init__.py
@@ -1,24 +1,31 @@
 from .models import Word
 from lxml import etree
 from urllib import urlencode
+from urllib2 import urlopen
 
 def declension(name):
-  try:
-    return Word.objects.get(nominative = name)
-  except Word.DoesNotExist:
-    elements = etree.parse('http://export.yandex.ru/inflect.xml?' + urlencode(
-      {
-        'name':name.encode('utf-8')
-      }
-    )).xpath('/inflections/inflection/text()')
-    if len(elements)>1:
-      return Word.objects.create(
-        nominative = name, 
-        genitive = unicode(elements[1]),
-        dative = unicode(elements[2]),
-        accusative = unicode(elements[3]),
-        instrumental = unicode(elements[4]),
-        prepositional = unicode(elements[5]),
-      )
-    else:
-      return None
+    try:
+        return Word.objects.get(nominative = name)
+    except Word.DoesNotExist:
+        try:
+            elements = etree.parse(urlopen('http://export.yandex.ru/inflect.xml?' + urlencode(
+              {
+                'name':name.encode('utf-8')
+              }
+            ), timeout = 2)).xpath('/inflections/inflection/text()')
+        except IOError:
+            # These *** banned us, so we will silently return nothing
+            # When ban will be over we can use it again
+            return None
+        if len(elements)>1:
+            return Word.objects.create(
+                nominative = name, 
+                genitive = unicode(elements[1]),
+                dative = unicode(elements[2]),
+                accusative = unicode(elements[3]),
+                instrumental = unicode(elements[4]),
+                prepositional = unicode(elements[5]),
+            )
+        else:
+            # TODO: Store unsuccessfull responses
+            return None
--- django_declension/models.py
+++ django_declension/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Word(models.Model):
-  nominative = models.CharField(max_length=255, unique = True)
-  genitive = models.CharField(max_length=255)
-  dative = models.CharField(max_length=255)
-  accusative = models.CharField(max_length=255)
-  instrumental = models.CharField(max_length=255)
-  prepositional = models.CharField(max_length=255)
+    nominative = models.CharField(max_length=255, unique = True)
+    genitive = models.CharField(max_length=255)
+    dative = models.CharField(max_length=255)
+    accusative = models.CharField(max_length=255)
+    instrumental = models.CharField(max_length=255)
+    prepositional = models.CharField(max_length=255)
+
+class DeclensionFail(models.Model):
+    word = models.CharField(max_length=255, unique = True)
