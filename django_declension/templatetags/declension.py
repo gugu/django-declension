b4720cca65d8087f6e0537a66aa85894cc566e6e
Ilya Yurtaev <ilya.yurtaev@gmail.com>
2012-02-10 18:17:15 +0600

    * Added exception handling in templatetag

--- django_declension/templatetags/declension.py
+++ django_declension/templatetags/declension.py
@@ -6,4 +6,9 @@ register = template.Library()
 @register.filter(name='declension')
 def declension_filter(name, inflection):
     word = declension(name)
-    return getattr(word, inflection)
+    try:
+        inflected_word = getattr(word, inflection)
+    except AttributeError:
+        inflected_word = name
+
+    return inflected_word
